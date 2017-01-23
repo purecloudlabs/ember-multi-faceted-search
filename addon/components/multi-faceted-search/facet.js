cf33df47987a45d5a64d1713ed216eb7cde6f5cd
Serge Margovsky <Serge.Margovsky@inin.com>
2017-01-23 11:07:53 -0600

    Issue 5: simplify selectedValues property of facet objects

--- addon/components/multi-faceted-search/facet.js
+++ addon/components/multi-faceted-search/facet.js
@@ -13,10 +13,8 @@ let Facet = Ember.Object.extend({
   atleastOneSelected: Ember.computed.gt('selected.length', 0),
   selectedValues: Ember.computed('multipleFacets', 'selected', function() {
     let terms = this.get('selected');
-    if (terms.length > 1) {
+    if (terms.length > 0) {
       return terms.map(t => t.value).join(", ");
-    } else if (terms.length === 1) {
-      return terms[0].value;
     } else {
       return "";
     }
