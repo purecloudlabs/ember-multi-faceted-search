87f8e4dcfba389b4ebee382a58f28ea0c30abad0
Tai, Naeem <Naeem.Tai@genesys.com>
2017-07-07 15:41:31 -0400

    Added a new ‘datetime’ facet type to show terms as date-time pickers.

--- addon/components/multi-faceted-search/facet.js
+++ addon/components/multi-faceted-search/facet.js
@@ -5,9 +5,11 @@ let Facet = Ember.Object.extend({
   type: "", // "checkbox" or "link"
   terms: [], // each term: { value: "", display: string.optional, count: num.optional, selected: boolean }
   emptyMessage: "Nothing to display",
+  hasError: false,
 
   checkbox: Ember.computed.equal('type', 'checkbox'),
   link: Ember.computed.equal('type', 'link'),
+  datetime: Ember.computed.equal('type', 'datetime'),
 
   selected: Ember.computed.filterBy('terms', 'selected', true),
   multipleSelected: Ember.computed.gt('selected.length', 1),
