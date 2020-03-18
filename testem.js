dd7e1d230b611fd50ada904a9156c55a8f8ed58f
Andy Kauffman <schlefty@gmail.com>
2020-03-18 15:46:17 -0400

    Update testem.js

--- testem.js
+++ testem.js
@@ -3,16 +3,3 @@ const config = require('ember-chromium').getTestemConfig();
 config.test_page = "tests/index.html?hidepassed";
 
 module.exports = config;
-/*jshint node:true*/
-// module.exports = {
-//   "framework": "qunit",
-//   "test_page": "tests/index.html?hidepassed",
-//   "disable_watching": true,
-//   "launch_in_ci": [
-//     "PhantomJS"
-//   ],
-//   "launch_in_dev": [
-//     "PhantomJS",
-//     "Chrome"
-//   ]
-// };
